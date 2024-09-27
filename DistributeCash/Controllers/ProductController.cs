a5fa5694af8abafb2d012c8eb409a749eb88ec03
Nazir13044 <rokoncse13044@gmail.com>
2024-09-27 17:04:28 +0600

    added data list in list

---
+++ DistributeCash/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using DistributedCash.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DistributedCash.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            ProductList productList = new ProductList();
+            var pro = productList.products();
+            return Ok(pro);
+        }
+    }
+}
---
+++ DistributeCash/Data/ProductList.cs
@@ -0,0 +1,24 @@
+﻿using DistributedCash.Model;
+
+namespace DistributedCash.Data;
+
+public class ProductList
+{
+    public List<Product> products()
+    { 
+     List<Product> productDatas = new List<Product>();
+
+        productDatas = new List<Product>()
+        {
+            new Product(){  ProductId=1, ProductName="Lux", ProductDescription="Beauty", Stock=52},
+            new Product(){  ProductId=2, ProductName="Sunsilk", ProductDescription="Shampo", Stock=10},
+            new Product(){  ProductId=3, ProductName="Pen", ProductDescription="Pen", Stock=01},
+            new Product(){  ProductId=4, ProductName="Mobile", ProductDescription="Electronics", Stock=56},
+            new Product(){  ProductId=5, ProductName="Laptop", ProductDescription="Electronics", Stock=20},
+            new Product(){  ProductId=6, ProductName="Plate", ProductDescription="Plate", Stock=31},
+            new Product(){  ProductId=7, ProductName="MoneyBag", ProductDescription="Money", Stock=25}
+        };
+
+        return productDatas;
+    }
+}
